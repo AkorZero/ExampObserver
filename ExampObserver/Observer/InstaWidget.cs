b22e1d35ee7ca2167ebb7249f2ffb86889a220ab
d.leminchuk <dimalemi@mail.ru>
2023-09-09 14:12:39 +0300

    All interfaces (+Insta, +Lenta)

---
+++ ExampObserver/Observer/InstaWidget.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampObserver.Observer
+{
+    internal class InstaWidget
+    {
+        string _photo;
+        public void Display()
+        {
+            Console.WriteLine($"Insta: {_photo}");
+        }
+
+        public void Update(string twitter, string lenta, string insta)
+        {
+            _photo = insta;
+            Display();
+        }
+    }
+}
